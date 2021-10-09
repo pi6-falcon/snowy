582ab877587b8bc25977c78a4a5b24dddd04e845
Rafan <rafanarbutis@outlook.com>
2021-10-09 17:01:11 -0300

    example how to add a route in app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'f', component: FirstComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
